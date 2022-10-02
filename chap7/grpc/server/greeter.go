786a215b4b50ca77c5ec06d0f0c5870e59c90838
Benjamin Kwon <siren.k.pro@gmail.com>
2022-10-02 15:18:37 +0900

    Go Programming Cookbook 2/e chapter 7. 웹 클라이언트와 API - gRPC 클라이언트 이해하기

---
+++ chap7/grpc/server/greeter.go
@@ -0,0 +1,24 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"golang/chap7/grpc/greeter"
+)
+
+// Greeter는 protoc에서 생성된 인터페이스를 구현한다.
+type Greeter struct {
+	Exclaim bool
+	greeter.UnimplementedGreeterServiceServer
+}
+
+// Greet 함수는 grpc Greet을 구현한다.
+func (g *Greeter) Greet(ctx context.Context, r *greeter.GreetRequest) (*greeter.GreetResponse, error) {
+	msg := fmt.Sprintf("%s %s", r.GetGreeting(), r.GetName())
+	if g.Exclaim {
+		msg += "!!"
+	} else {
+		msg += ".."
+	}
+	return &greeter.GreetResponse{Response: msg}, nil
+}
---
+++ cmd/chap7/grpc/client/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"golang/chap7/grpc/greeter"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial(":4444", grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	client := greeter.NewGreeterServiceClient(conn)
+	ctx := context.Background()
+	req := greeter.GreetRequest{Greeting: "Hello", Name: "Reader"}
+	resp, err := client.Greet(ctx, &req)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(resp)
+
+	req.Greeting = "Goodbye"
+	resp, err = client.Greet(ctx, &req)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(resp)
+}
